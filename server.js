98fb90971444d4ffc409b40b9e0deeb7ecf36c26
adaojreess <adaojreess@gmail.com>
2020-06-10 20:34:16 -0300

    Refactor

--- server.js
+++ server.js
@@ -1,55 +1,54 @@
 require('dotenv/config')
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
+const server = require('http').createServer();
 const io = require('socket.io')(server);
 const firebase = require('./src/firebase');
 
-app.use('/', (req, res) => {
-    return res.json({ message: 'ON' });
-});
-
-var allSchedules = [];
-
-io.on('connect', socket => {
+io.on('connection', socket => {
+    var allSchedules = [];
     console.log('Socket: ' + socket.id);
+
     socket.on('createSchedule', data => {
+        data.start = new Date(data.start);
         try {
             firebase.firestore()
-                .collection('schedulings')
+                .collection('scheduling')
                 .doc(data.cpf)
                 .set(data).then(() => {
                     socket.emit('message', 'Evento Criado!');
                 });
         } catch (e) {
-            socket.emit('message', 'Evento não Criado!');
+            socket.emit('message', 'Evento não criado');
         }
     });
 
-    firebase.firestore().collection('schedulings').onSnapshot(function (querySnapshot) {
-        var schedule = [];
+    firebase.firestore().collection('scheduling').onSnapshot(function (querySnapshot) {
+        var schedules = [];
         querySnapshot.forEach(function (doc) {
-            schedule.push(doc.data());
+            schedules.push(doc.data());
         });
-        allSchedules = schedule;
-        socket.emit('getSchedulings', schedule)
+        allSchedules = schedules;
+        socket.emit('getScheduling', schedules);
     });
-    
-    //[yyyy-mm-dd, city]
-    socket.on('getDate', data => {
+
+
+    const emitList = (data) => {
         var [date, city] = data;
         date = new Date(date);
         var list = [];
         allSchedules.forEach(schedule => {
-            if (schedule.city === city) {
+            if (schedule.city == city) {
                 var start = new Date(schedule.start.seconds * 1000);
                 if (date.getDate() + 1 === start.getDate() && date.getMonth() === start.getMonth()) {
-                    list.push(start.getHours().toString().padStart(2, "0") + ":" + start.getMinutes().toString().padStart(2, "0"));
+                    list.push(start.getHours().toString().padStart(2, '0') + start.getMinutes().toString().padStart(2, '0'));
                 }
             }
         });
+
         socket.emit('getListHours', list);
-    });
+    }
+
+    //[yyyy-mm-dd, city]
+    socket.on('checkDate', emitList);
 });
 
 server.listen(process.env.PORT || 3000);
