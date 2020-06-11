ea80be76f2a500eb5669e416bbf73243262aa62d
adaojreess <adaojreess@gmail.com>
2020-06-11 15:54:19 -0300

    Refactor

--- server.js
+++ server.js
@@ -8,27 +8,38 @@ io.on('connection', socket => {
     console.log('Socket: ' + socket.id);
 
     socket.on('createSchedule', data => {
+        var verify = true;
         data.start = new Date(data.start);
-        try {
-            firebase.firestore()
-                .collection('scheduling')
-                .doc(data.cpf)
-                .set(data).then(() => {
-                    socket.emit('message', 'Evento Criado!');
-                });
-        } catch (e) {
-            socket.emit('message', 'Evento não criado');
-        }
+        allSchedules.forEach(schedule => {
+            var start = new Date(schedule.start.seconds * 1000);
+            var checkDate = start.getDate() + start.getHours() + start.getMinutes() === data.start.getDate() + data.start.getHours() + data.start.getMinutes();
+            if (schedule.city === data.city && checkDate && verify) verify = false;
+        });
+        if (verify) {
+            console.log('salva')
+            try {
+                firebase.firestore()
+                    .collection('scheduling')
+                    .doc(data.cpf)
+                    .set(data).then(() => {
+                        socket.emit('message', 'Evento Criado!');
+                    });
+            } catch (e) {
+                socket.emit('message', 'Evento não criado');
+            }
+         } else  socket.emit('message', 'Horario não dispnível');
+
     });
 
-    firebase.firestore().collection('scheduling').onSnapshot(function (querySnapshot) {
-        var schedules = [];
-        querySnapshot.forEach(function (doc) {
-            schedules.push(doc.data());
+    firebase.firestore().collection('scheduling').onSnapshot(
+        function (querySnapshot) {
+            var schedules = [];
+            querySnapshot.forEach(function (doc) {
+                schedules.push(doc.data());
+            });
+            allSchedules = schedules;
+            socket.emit('schedules', schedules);
         });
-        allSchedules = schedules;
-        socket.emit('getScheduling', schedules);
-    });
 
 
     const emitList = (data) => {
@@ -39,15 +50,14 @@ io.on('connection', socket => {
             if (schedule.city == city) {
                 var start = new Date(schedule.start.seconds * 1000);
                 if (date.getDate() + 1 === start.getDate() && date.getMonth() === start.getMonth()) {
-                    list.push(start.getHours().toString().padStart(2, '0') + start.getMinutes().toString().padStart(2, '0'));
+                    list.push(start.getHours().toString().padStart(2, '0') + ':' + start.getMinutes().toString().padStart(2, '0'));
                 }
             }
         });
-
-        socket.emit('getListHours', list);
+        socket.emit('listInvalidTimes', list);
     }
 
-    //[yyyy-mm-dd, city]
+    // [yyyy-mm-dd, city]
     socket.on('checkDate', emitList);
 });
 
