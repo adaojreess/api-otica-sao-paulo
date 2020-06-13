92e7e3321667e477ba4776d94bb834b03d86b74f
adaojreess <adaojreess@gmail.com>
2020-06-13 02:24:16 -0300

    Add spreadsheet

--- server.js
+++ server.js
@@ -1,77 +1,95 @@
-require('dotenv/config')
+require('dotenv/config');
 const server = require('http').createServer();
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+    origins: process.env.ORIGIN,
+});
 const firebase = require('./src/firebase');
+const spredsheet = require('./src/spreadsheet');
+
+var allSchedules = [];
+
+var emitSchedules = () => { };
+
+firebase.firestore().collection('scheduling').onSnapshot(querySnapshot => {
+    var schedules = [];
+    querySnapshot.forEach(function (doc) {
+        schedules.push(doc.data());
+    });
+    allSchedules = schedules;
+
+    console.log(schedules);
+
+    emitSchedules();
+});
 
 const emitCalendar = () => {
     list = [];
     var date = new Date();
     for (; list.length < 15;) {
         date = new Date(date.setTime(date.getTime() + 1 * 86400000));
-        if (date.getDay() !== 0 && date.getDay() !== 6) list.push(date.getDate());
+        if (date.getDay() !== 0 && date.getDay() !== 6) list.push(date);
     }
-    
     return list;
 }
 
+const createSchedule = (data, callback) => {
+    var verify = true;
+    data.start = new Date(data.start);
+    allSchedules.forEach(schedule => {
+        var start = new Date(schedule.start.seconds * 1000);
+        var checkDate = start.getDate() + start.getHours() + start.getMinutes() === data.start.getDate() + data.start.getHours() + data.start.getMinutes();
+        if (schedule.city === data.city && checkDate && verify) verify = false;
+    });
+    if (verify) {
+        try {
+            firebase.firestore()
+                .collection('scheduling')
+                .doc(data.cpf)
+                .set(data).then(() => {
+                    callback('Visita agendada!');
+                    spredsheet.addScheduleToSheet(data).then(result => console.log(result));
+                });
+        } catch (e) {
+            callback('Visita não agendada, tente novamente.');
+        }
+    } else callback('Visita não agendada, horário não disponível');
+}
+
+const emitListInvalidTimes = (data, callback) => {
+    var [date, city] = data;
+    date = new Date(date);
+    var list = [];
+
+    allSchedules.filter(value => value.city === city).map(schedule => {
+        var start = new Date(schedule.start.seconds * 1000);
+        if (date.getDate() + 1 === start.getDate() && date.getMonth() === start.getMonth()) {
+            list.push(start.getHours().toString().padStart(2, '0') + ':' + start.getMinutes().toString().padStart(2, '0'));
+        }
+    });
+
+    callback(list);
+}
+
 io.on('connection', socket => {
-    var allSchedules = [];
     console.log('Socket: ' + socket.id);
 
     socket.emit('calendar', emitCalendar());
+    emitSchedules = () => { socket.emit('schedules', allSchedules); };
+
+    emitSchedules();
 
     socket.on('createSchedule', data => {
-        var verify = true;
-        data.start = new Date(data.start);
-        allSchedules.forEach(schedule => {
-            var start = new Date(schedule.start.seconds * 1000);
-            var checkDate = start.getDate() + start.getHours() + start.getMinutes() === data.start.getDate() + data.start.getHours() + data.start.getMinutes();
-            if (schedule.city === data.city && checkDate && verify) verify = false;
+        createSchedule(data, (message) => {
+            socket.emit('message', message);
         });
-        if (verify) {
-            console.log('salva')
-            try {
-                firebase.firestore()
-                    .collection('scheduling')
-                    .doc(data.cpf)
-                    .set(data).then(() => {
-                        socket.emit('message', 'Evento Criado!');
-                    });
-            } catch (e) {
-                socket.emit('message', 'Evento não criado');
-            }
-        } else socket.emit('message', 'Horario não dispnível');
-
     });
 
-    firebase.firestore().collection('scheduling').onSnapshot(
-        function (querySnapshot) {
-            var schedules = [];
-            querySnapshot.forEach(function (doc) {
-                schedules.push(doc.data());
-            });
-            allSchedules = schedules;
-            socket.emit('schedules', schedules);
-        });
-
-    const emitList = (data) => {
-        var [date, city] = data;
-        date = new Date(date);
-        var list = [];
-
-        list = allSchedules.filter(value => value.city === city).map(schedule => {
-            var start = new Date(schedule.start.seconds * 1000);
-            if (date.getDate() + 1 === start.getDate() + 1 && date.getMonth() === start.getMonth()) {
-                return (start.getHours().toString().padStart(2, '0') + ':' + start.getMinutes().toString().padStart(2, '0'))
-            }
-        });
-        socket.emit('listInvalidTimes', list);
-    }
-
     // [yyyy-mm-dd, city]
-    socket.on('checkDate', emitList);
+    socket.on('checkDate', data => {
+        emitListInvalidTimes(data, (list) => {
+            socket.emit('listInvalidTimes', list);
+        });
+    });
 });
 
-emitCalendar();
-
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 4000);
---
+++ src/spreadsheet/index.js
@@ -0,0 +1,81 @@
+const GoogleSpreadsheet = require('google-spreadsheet');
+const { promisify } = require('util');
+const creds = require('../../client_secret');
+const moment = require('moment');
+
+const addScheduleToSheet = async (data) => {
+    moment.locale('pt-br');
+
+    const sheetIndex = data.city === "Piripiri" ? 0 : 1;
+    const doc = new GoogleSpreadsheet('1v6s0VWlANH2PtjSuX_g9uZebqwZDHlNuVqrnVOATNY0');
+    await promisify(doc.useServiceAccountAuth)(creds);
+    const info = await promisify(doc.getInfo)();
+    const sheet = info.worksheets[sheetIndex];
+
+    data.start = moment(data.start).format('LLL');
+
+
+    data = {
+        Nome: data.name,
+        Horario: data.start,
+        CPF: data.cpf,
+        Email: data.email,
+        Telefone: data.phone,
+        Cidade: data.city
+    }
+
+    await promisify(sheet.addRow)(data).then(() => {
+        return "success";
+    }).catch(e => {
+        return "error";
+    });
+}
+
+async function removeSchedule(data) {
+    const sheetIndex = data.city === "Piripiri" ? 0 : 1;
+    const doc = new GoogleSpreadsheet('1v6s0VWlANH2PtjSuX_g9uZebqwZDHlNuVqrnVOATNY0');
+    await promisify(doc.useServiceAccountAuth)(creds);
+    const info = await promisify(doc.getInfo)();
+    const sheet = info.worksheets[sheetIndex];
+
+    try {
+        const rows = await promisify(sheet.getRows)({
+            query: 'cpf = ' + data.cpf
+        });
+        rows[0].del();
+        return "success";
+    } catch (e) {
+        return `message: ${e}`;
+    }
+}
+
+async function updateShedule(data) {
+    const sheetIndex = data.city === "Piripiri" ? 0 : 1;
+
+    const doc = new GoogleSpreadsheet('1v6s0VWlANH2PtjSuX_g9uZebqwZDHlNuVqrnVOATNY0');
+    await promisify(doc.useServiceAccountAuth)(creds);
+    const info = await promisify(doc.getInfo)();
+    const sheet = info.worksheets[sheetIndex];
+
+    try {
+        const rows = await promisify(sheet.getRows)({
+            query: 'cpf = ' + data.cpf
+        });
+
+        rows[0].name = data.name;
+        rows[0].start = data.start;
+        rows[0].email = data.email;
+        rows[0].phone = data.phone;
+
+        rows[0].save();
+        return "success";
+    } catch (e) {
+        return `message: ${e}`;
+    }
+}
+
+module.exports = {
+    addScheduleToSheet,
+    removeSchedule,
+    updateShedule
+};
