8483624489d7a287c55a3963e30fb0a2155df2ca
adaojreess <adaojreess@gmail.com>
2020-06-13 23:45:57 -0300

    Express implemented

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 require('dotenv/config');
-const server = require('http').createServer();
-const io = require('socket.io')(server, { origins: process.env.ORIGINS });
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const routes = express.Router();
+
 const firebase = require('./src/firebase');
 const spredsheet = require('./src/spreadsheet');
-const moment = require('moment');
 
-moment.locale('pt-br');
 var allSchedules = [];
-var emitSchedules = () => { };
 
 const listTimes = ["07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30",
 "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30",
-"17:00", "17:30", "18:00"]
-const listTimesSat = [
-    "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30",
-    "12:00"
-]
+"17:00", "17:30", "18:00"];
 
 firebase.firestore().collection('scheduling').onSnapshot(querySnapshot => {
     var schedules = [];
@@ -23,11 +25,9 @@ firebase.firestore().collection('scheduling').onSnapshot(querySnapshot => {
         schedules.push(doc.data());
     });
     allSchedules = schedules;
-
-    emitSchedules();
 });
 
-const emitCalendar = () => {
+app.use(routes.get('/calendar', (req, res) => {
     list = [];
     var date = new Date();
     for (; list.length < 15;) {
@@ -36,34 +36,12 @@ const emitCalendar = () => {
             list.push(`${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${(date.getDate()).toString().padStart(2, '0')}`);
         }
     }
-    return list;
-}
+    res.json({calendar: list});
+}));
 
-const createSchedule = (data, callback) => {
-    var verify = true;
-    data.start = new Date(data.start);
-    allSchedules.forEach(schedule => {
-        var start = new Date(schedule.start.seconds * 1000);
-        var checkDate = start.getDate() + start.getHours() + start.getMinutes() === data.start.getDate() + data.start.getHours() + data.start.getMinutes();
-        if (schedule.city === data.city && checkDate && verify) verify = false;
-    });
-    if (verify) {
-        try {
-            firebase.firestore()
-                .collection('scheduling')
-                .doc(data.cpf)
-                .set(data).then(() => {
-                    callback('Visita agendada!');
-                    spredsheet.addScheduleToSheet(data).then(result => console.log(result));
-                });
-        } catch (e) {
-            callback('Visita não agendada, tente novamente.');
-        }
-    } else callback('Visita não agendada, horário não disponível');
-}
+app.use(routes.get('/schedules', (req, res) => {
+    var {date, city} = req.body;
 
-const emitScheduleList = (data, callback) => {
-    var [date, city] = data;
     date = new Date(date);
     var list = [];
 
@@ -80,34 +58,38 @@ const emitScheduleList = (data, callback) => {
             if (!list.includes(element)) newList.push(element);
         });
     } else {
-        listTimesSat.forEach(element => {
+        listTimes.slice(0, 10).forEach(element => {
             if (!list.includes(element)) newList.push(element);
         });
     }
 
-    callback(newList);
-}
+    res.json({schedules: newList});
+}));
 
-io.on('connection', socket => {
-    console.log('Socket: ' + socket.id);
-
-    socket.emit('calendar', emitCalendar());
-    emitSchedules = () => { socket.emit('schedules', allSchedules); };
-
-    emitSchedules();
-
-    socket.on('createSchedule', data => {
-        createSchedule(data, (message) => {
-            socket.emit('message', message);
-        });
+app.use(routes.post('/schedule' , (req, res) => {
+    var data = req.body;
+    
+    var verify = true;
+    data.start = new Date(data.start);
+    allSchedules.forEach(schedule => {
+        var start = new Date(schedule.start.seconds * 1000);
+        var checkDate = start.getDate() + start.getHours() + start.getMinutes() === data.start.getDate() + data.start.getHours() + data.start.getMinutes();
+        if (schedule.city === data.city && checkDate && verify) verify = false;
     });
+    if (verify) {
+        try {
+            firebase.firestore()
+                .collection('scheduling')
+                .doc(data.cpf)
+                .set(data).then(() => {
+                    spredsheet.addScheduleToSheet(data).then(result => res.json({ message: "success" }));
+                });
+        } catch (e) {res.json({message: "error"})};
+    } else res.json({message: "impossible schedule"})
+}));
 
-    // [yyyy-mm-dd, city]
-    socket.on('checkDate', data => {
-        emitScheduleList(data, (list) => {
-            socket.emit('scheduleList', list);
-        });
-    });
+app.use('/', (req, res) => {
+    res.json({message: " API online"})
 });
 
-server.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 4000);
