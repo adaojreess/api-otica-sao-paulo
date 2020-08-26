7b2d802f6d0d1f6434916c3ea1cdd371cd075e56
adaojreess <adaojreess@gmail.com>
2020-08-26 19:22:29 -0300

    add check time

--- src/controllers/AppointmentController.js
+++ src/controllers/AppointmentController.js
@@ -1,14 +1,13 @@
 const moment = require('moment');
 const firebase = require('../firebase');
 const spreadsheet = require('../spreadsheet');
-
 const generateAppointmentsWithId = require('../utils/generateAppointmentsWithId');
 const removeDocument = require('../utils/removeDocument');
-
 const searchByCpf = require('../utils/searchByCpf');
 const searchById = require('../utils/searchById');
 const generateCalendar = require('../utils/generateCalendar');
 const generateSchedules = require('../utils/generateSchedules');
+const checkTime = require('../utils/checkTime');
 
 let appointmentListPedroII = [];
 let appointmentListPiripiri = [];
@@ -39,7 +38,6 @@ firebase.firestore().collection('cities')
         appointmentListPedroII = schedule;
     });
 
-
 module.exports = {
     index(req, res) {
         const filters = req.query;
@@ -51,52 +49,33 @@ module.exports = {
 
         return res.json(list);
     },
+
     async create(req, res) {
         let data = req.body;
-        let verify = true;
         let date = moment(data.start).utc();
-
-        let id;
-
-        let message = "Horário indisponivel";
+        let id = date.valueOf();
 
         try {
-            let list = data.city === "Piripiri" ? appointmentListPiripiri : appointmentListPedroII;
-
-            list.forEach(appointment => {
-                let start = moment.unix(appointment.start.seconds);
-                let checkDate = start.valueOf() === date.valueOf();
-
-                if (verify && (appointment.cpf === data.cpf || checkDate)) {
-                    verify = false;
-                    if (appointment.cpf === data.cpf) message = "CPF já cadastrado";
-                }
-            });
+            let { verify, message } = checkTime(data.city === "Piripiri" ? appointmentListPiripiri : appointmentListPedroII, data.cpf, date);
 
             if (verify) {
-                id = date.valueOf();
-                data.start = date.toDate()
+                data.start = date.toDate();
 
-                await firebase.firestore()
-                    .collection('cities')
-                    .doc(data.city)
-                    .collection('schedules')
-                    .doc(id.toString())
-                    .set({ ...data, id });
+                await firebase.firestore().collection('cities')
+                    .doc(data.city).collection('schedules')
+                    .doc(id.toString()).set({ ...data, id });
                 await spreadsheet.addScheduleToSheet({ ...data, id });
             } else {
-                res.statusCode = 500;
-                return res.json({ message: message });
+                return res.status(500).json({ message: message });
             }
         } catch (e) {
-            res.statusCode = 500;
-            return res.json({ "message": "Erro ao salvar dados", "error": e });
+            return res.status(500).json({ "message": "Erro ao salvar dados", "error": e });
         }
         return res.json({ message: "Visita salva", id });
     },
+
     async edited(req, res) {
         const data = req.body;
-
         let newId = moment(data.start).valueOf();
 
         data.start = new Date(data.start);
@@ -145,6 +124,7 @@ module.exports = {
         }
         return res.json({ "message": "Dados atualizados" });
     },
+
     async remove(req, res) {
         let id = req.query.id;
         let city = req.query.city;
@@ -159,6 +139,7 @@ module.exports = {
 
         res.json({ "message": "Dados deletados" });
     },
+
     calendar(req, res) {
         const city = req.query.city
         const date = req.query.date !== undefined ? moment(req.query.date).utc() : undefined;
@@ -169,6 +150,7 @@ module.exports = {
             return res.json(generateSchedules(city === "Piripiri" ? appointmentListPiripiri : appointmentListPedroII, date));
         }
     },
+
     search(req, res) {
         const cpf = req.query.cpf;
         const city = req.query.city;
